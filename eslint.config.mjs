b08976cc964c3ab74d4ee3f41be5809b502db6c0
Jay Gogad <jay@thenervana.com>
2025-08-01 21:29:41 +0530

    Disable breaking ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-export default {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+export default [
+  ...compat.extends("next"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off"
+    }
+  }
+];
