9747a674af349c27884d1d639826af8f31ceb59d
Jay Gogad <jay@thenervana.com>
2025-08-01 21:48:32 +0530

    Temporarily disable ESLint during build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  reactStrictMode: true,
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
+
+module.exports = nextConfig;
